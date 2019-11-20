22756bfd58c97bece49200b75f2aa4c687896b01
Angeliki Patrinou <36475005+patrinoua@users.noreply.github.com>
2019-11-20 12:15:23 +0100

    Update createTicketName.js

--- createTicketName.js
+++ createTicketName.js
@@ -1,23 +1,24 @@
 
 function renameTicket(ticketName){
-
-  const usefulCharacters = [...ticketName.split(' FE:').join()]
-
+  const usefulCharacters = [...ticketName.substr(10).trim()]
   const newTicketName = usefulCharacters.map(character=>{
     if(character === ' ')
-      return '_'
+      return '-'
     else if(
       character === ':'
       || character === '['
       || character === ']'
       || character === '.'
       || character === ','
+      || character === '-'
+      || character === '('
+      || character === ')'
+      || character === '#'
       || character === '\n' )
       return ''
-
-    return character
+    return character.toLowerCase()
   })
-  return newTicketName.join('')
+  return [ticketName.substr(0,9)]+'-'+newTicketName.join('').substr(0,40)
 }
 
 const gitCompatibleName = renameTicket('\n\nTAS-63 FE: Hub - Adjust color of Order creation Trade Terms sentences.\n\n')
