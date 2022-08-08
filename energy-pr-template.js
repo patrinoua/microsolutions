4fbad44fb5b17e2b10a980edbb313d7807e58994
Angela Patrinou <patrinoua@gmail.com>
2022-08-08 18:28:57 +0200

    use prompt

--- energy-pr-template.js
+++ energy-pr-template.js
@@ -1,20 +1,24 @@
+var prompt = require('prompt-sync')()
+
+const name = prompt("what's the ticket name? ")
+
 function produceBranchAndPR(ticketName) {
   let nameArray = [...ticketName]
 
-  const removeNewLine = nameArray.filter((character) => character !== "\n")
+  const removeNewLine = nameArray.filter((character) => character !== '\n')
 
   nameArray = removeNewLine
 
-  const branchNameArray = nameArray.map(character => {
+  const branchNameArray = nameArray.map((character) => {
     if (character === ' ') return '-'
     else return character.toLowerCase()
   })
 
   const branchName = branchNameArray.join('')
-  let ticketNumber = ticketName.split(" ")[0]
+  let ticketNumber = ticketName.split(' ')[0].trim()
   let ticketTitle = nameArray.slice(11, nameArray.length).join('')
 
-  const prName = 'feat(' + ticketNumber + "): " + ticketTitle
+  const prName = 'WIP feat(' + ticketNumber + '): ' + ticketTitle
   const thisPrResolves = `This PR resolves [${ticketNumber}]`
   const PRTemplate = `
 <!-- PR title in form of \`type(PROJECT - XXXX): what changed\`
@@ -27,33 +31,29 @@ ${thisPrResolves}
 Follow up: add the right link, updates to metaphysics are required.
 
 ### PR Checklist
-
 - [ ] I tested my changes on **iOS** and **Android**.
 - [ ] I added screenshots or videos to illustrate my changes.
 - [ ] I added Tests and Stories for my changes.
-
 ### To the reviewers 👀
-
 - [ ] I would like **at least one** of the reviewers to run this PR on the simulator or device.
-
 
 Need help with something? Have a look at our [docs], or get in touch with us.
 
 [docs]: /docs/README.md
-
-[${ticketNumber}]: https://artsyproduct.atlassian.net/browse/${ticketNumber}
 `
 
-  console.log('\n\n\n\n - - - - - - - - - PR Description - - - - - - - - - \n\n')
+  console.log(
+    '\n\n\n\n - - - - - - - - - PR Description - - - - - - - - - \n\n'
+  )
   console.log(PRTemplate)
   console.log('\n\n\n - - - - - - - - -PR name - - - - - - - - - - - - \n\n\n')
   console.log(prName)
   console.log('\n\n\n\n - - - - - - - - - branch name- - - - - - - - - - \n\n')
   console.log(branchName)
   console.log('\n\n\n')
-
 }
 
-const ticketName = "LALA-231 Create three tab root structure"
+// const ticketName = `
+// MOPLAT-335 Artwork: Open artwork in CMS`
 
-console.log(produceBranchAndPR(ticketName))
+console.log(produceBranchAndPR(name))
