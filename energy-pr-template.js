e97d5ad8f7b1add3cd1ea9cf1dd5c75c9e3d5e19
Angela Patrinou <patrinoua@gmail.com>
2022-08-31 11:42:45 +0200

    Update energy-pr-template.js

--- energy-pr-template.js
+++ energy-pr-template.js
@@ -1,24 +1,34 @@
-var prompt = require('prompt-sync')()
+var prompt = require("prompt-sync")()
 
+console.log("\n\n\n")
 const name = prompt("what's the ticket name? ")
+console.log("\n\n\n")
 
 function produceBranchAndPR(ticketName) {
   let nameArray = [...ticketName]
 
-  const removeNewLine = nameArray.filter((character) => character !== '\n')
+  const removeNewLine = nameArray.filter((character) => character !== "\n")
 
   nameArray = removeNewLine
 
   const branchNameArray = nameArray.map((character) => {
-    if (character === ' ') return '-'
+    if (character === " ") return "-"
     else return character.toLowerCase()
   })
 
-  const branchName = branchNameArray.join('')
-  let ticketNumber = ticketName.split(' ')[0].trim()
-  let ticketTitle = nameArray.slice(11, nameArray.length).join('')
+  // console.log(branchNameArray)
 
-  const prName = 'WIP feat(' + ticketNumber + '): ' + ticketTitle
+  const branchName = branchNameArray
+    .filter((a) => a !== "'")
+    .join("")
+    .slice(0, 40)
+
+  const checkoutBranchName = "gco -b patrinoua/" + branchName
+  let ticketNumber = ticketName.split(" ")[0].trim()
+  let ticketTitle = nameArray.slice(11, nameArray.length).join("")
+
+  const prNameWIP = "WIP feat(" + ticketNumber + "): " + ticketTitle
+  const prNameREADY = "feat(" + ticketNumber + "): " + ticketTitle
   const thisPrResolves = `This PR resolves [${ticketNumber}]`
   const PRTemplate = `
 <!-- PR title in form of \`type(PROJECT - XXXX): what changed\`
@@ -27,8 +37,6 @@ function produceBranchAndPR(ticketName) {
 <!-- Info, implementation, how to get there, before & after screenshots & videos, follow-up work, etc -->
 
 ${thisPrResolves}
-
-Follow up: add the right link, updates to metaphysics are required.
 
 ### PR Checklist
 - [ ] I tested my changes on **iOS** and **Android**.
@@ -42,17 +50,19 @@ Need help with something? Have a look at our [docs], or get in touch with us.
 [docs]: /docs/README.md
 `
 
-  console.log(
-    '\n\n\n\n - - - - - - - - - PR Description - - - - - - - - - \n\n'
-  )
+  console.log("\n\n\n\n - - - - - - - - - PR Description - - - - - - - - - \n")
   console.log(PRTemplate)
-  console.log('\n\n\n - - - - - - - - -PR name - - - - - - - - - - - - \n\n\n')
-  console.log(prName)
-  console.log('\n\n\n\n - - - - - - - - - branch name- - - - - - - - - - \n\n')
-  console.log(branchName)
-  console.log('\n\n\n')
+  console.log("\n\n - - - - - - - - - - - PR name - - - - - - - - - - - - - - - - - ")
+  console.log("\n >>>> WIP \n")
+  console.log(prNameWIP)
+  console.log("\n\n >>>> READY \n")
+  console.log(prNameREADY)
+  // console.log("\n\n - - - - - - - - - Create PR with branchName - - - - - - - - - - \n\n")
+  // console.log("git push --set-upstream origin ", branchName)
+  console.log("\n\n\n - - - - - - - - - Branch name- - - - - - - - - - - - - - - - - - \n\n")
+  console.log(checkoutBranchName)
+  console.log("\n\n")
 }
-
 // const ticketName = `
 // MOPLAT-335 Artwork: Open artwork in CMS`
 
